020788becd16bc2247ea507578a0e01755909746
- <montecarlo1101@gmail.com>
2026-05-05 18:27:11 +0900

    introduce biome (#4)

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 /* @refresh reload */
-import { render } from "solid-js/web";
-import App from "./App";
+import { render } from 'solid-js/web'
+import App from './App'
 
-render(() => <App />, document.getElementById("root") as HTMLElement);
+const root = document.getElementById('root')
+if (!root) throw new Error('root element not found')
+
+render(() => <App />, root)
